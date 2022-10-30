d28873b81fba44d1d6710154557e05cc454797af
seunope <seunope2004@yahoo.co.uk>
2022-10-30 14:06:28 +0100

    validation

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,30 +1,30 @@
-import express, { Request, Response } from "express";
-import { body, validationResult } from "express-validator";
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 
 const router = express.Router();
 
 router.post(
-  "/api/users/signup",
+  '/api/users/signup',
   [
-    body("email").isEmail().withMessage("Email must be valid"),
-    body("password")
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password')
       .trim()
       .isLength({ min: 4, max: 20 })
-      .withMessage("Password must be between 4 and 20 characters"),
+      .withMessage('Password must be between 4 and 20 characters'),
   ],
   (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      return res.status(400).send(errors.array());
+      // This is javascript land (not ts)
+      const error = new Error('Invalid email or password');
+      //error.reasons = errors.array();
+      throw error;
     }
-    const { email, password } = req.body;
-
-    console.log("Creating a user...");
+    console.log('Creating a user...');
+    throw new Error('Error connecting to database');
 
     res.send({});
-
-    // new User({ email, password })
   }
 );
 
